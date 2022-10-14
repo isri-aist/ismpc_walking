f67e93608fb6052d98c42e3e47a66cdbc0773318
antodld <antonin.dallard@lirmm.fr>
2022-10-14 15:32:10 +0900

    Changed cstr param

--- src/Walking_controller.cpp
+++ src/Walking_controller.cpp
@@ -380,8 +380,8 @@ bool Walking_controller::run()
 
     //Ax+b>=0
   planes_ =  {
-    {{0., 0.,  -1.},  (controller_config_.Stab_config.comHeight + 0.01) },
-    {{0., 0.,   1.}, -(controller_config_.Stab_config.comHeight - 0.01) }
+    {{0., 0.,  -1.},  (controller_config_.Stab_config.comHeight + 0.02) },
+    {{0., 0.,   1.}, -(controller_config_.Stab_config.comHeight - 0.02) }
   };
   
 
@@ -410,7 +410,7 @@ bool Walking_controller::run()
 
   count += 1;
 
-  comIncPlaneConstraintPtr_->setPlanes(solver(), planes_);
+  comIncPlaneConstraintPtr_->setPlanes(solver(), planes_,{},{},1e-2,1e-3);
 
 
   bool ret = mc_control::fsm::Controller::run();
