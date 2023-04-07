ba5944b6e19252992523bef4a2fd13d22a74cafe
adallard <antonin.dallard@lirmm.fr>
2023-04-07 11:21:10 +0900

    MPC thread priority for real time system

--- src/Walking_controller.cpp
+++ src/Walking_controller.cpp
@@ -202,16 +202,20 @@ bool Walking_controller::wait_for_mpc_thread()
       WalkingTrajectory_Computing = true;
       MPC_thread_on = true;
       WalkingTrajectoryThread = std::thread(&Walking_controller::WalkingTrajectoryLoop, this);
-      auto th_handle = WalkingTrajectoryThread.native_handle();
-      int policy = 0;
-      sched_param param{};
-      pthread_getschedparam(th_handle, &policy, &param);
-      mc_rtc::log::info("MPC thread priority: {}", param.sched_priority);
-      if(param.sched_priority > 90)
-      {
+      #ifndef WIN32
+        // Lower thread priority so that it has a lesser priority than the real time
+        // thread
+        auto th_handle = WalkingTrajectoryThread.native_handle();
+        int policy = 0;
+        sched_param param{};
+        pthread_getschedparam(th_handle, &policy, &param);
         param.sched_priority = 80;
-        pthread_setschedparam(th_handle, SCHED_RR, &param);
-      }
+        if(pthread_setschedparam(th_handle, SCHED_RR, &param) != 0)
+        {
+          mc_rtc::log::warning("[{}] Failed to lower calibration thread priority. If you are running on a real-time system, "
+                              "this might cause latency to the real-time loop.");
+        }
+      #endif
 
       compute_trajectory_once.notify_all();
     }
